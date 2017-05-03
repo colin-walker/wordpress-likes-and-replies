2626c7798d53c229163589ff0299d47f4136d4d7
colin-walker <colin@colinwalker.me.uk>
2017-05-03 22:49:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# wordpress-likes-and-replies
+
+Add IndieWeb likes and replies to posts with microformat2 markup using the value from custom fields
+
+Usage: Ideal for posting from outside of WordPress, e.g. from Workflow on iOS. Add the required page URL to a custom filed called 'Liked' or 'Reply' to have it automatically added to the post body. Microformat2 markup ensures the relevant webmention is sent on posting.
+
+   	
---
+++ wordpress-likes-and-replies.php
@@ -0,0 +1,45 @@
+<?php
+
+	/*
+
+   	Plugin Name: Likes and replies from custom fields
+
+   	Description: Add IndieWeb likes and replies to posts with microformat2 markup using the value from custom fields
+
+   	Version: 0.1
+
+   	Author: Colin Walker
+
+	*/
+
+	function mentiontypes ( $content ) {
+
+  		$id = get_the_ID();
+  		$types = array ( 'Reply', 'Liked' );
+
+  		foreach ( $types as $type) {
+    		$mentionurl = (get_post_meta($id, $type, true));
+
+    		if ( $mentionurl !="" ) {
+     			$url = wp_remote_get($mentionurl);
+      			$str = wp_remote_retrieve_body( $url );
+      			$str = trim(preg_replace('/\s+/', ' ', $str));
+      			preg_match("/\<title\>(.*)\<\/title\>/i",$str,$mentontitle);
+
+      			if ( $type == 'Reply' ) {
+        			$mentionstr = '<p><em>In reply to: <a class="u-in-reply-to" href="' . $mentionurl . '">' . $mentontitle[1] . '</a>...</em></p>';
+      			} else {
+        			$mentionstr = '<p><em>Liked: <a class="u-like-of" href="' . $mentionurl . '">' . $mentontitle[1] . '</a>...</em></p>';
+      			}
+
+      			$content = $mentionstr . $content ;
+      			delete_post_meta( $id, $type, $mentionurl );
+    		}
+  		}
+
+  		return $content;  
+	}
+
+	add_filter( 'content_save_pre', 'mentiontypes' );
+
+?>
