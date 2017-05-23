c44a6f007efd85438d182569db4f77977cbf8ae2
Mobile User <mobile@iPhone>
2017-05-23 19:45:28 +0100

    Updated for posts made via REST API

--- wordpress-likes-and-replies.php
+++ wordpress-likes-and-replies.php
@@ -8,12 +8,11 @@
 
    	Description: Add IndieWeb likes and replies to posts with microformat2 markup using the value from custom fields
 
-   	Version: 0.3
+   	Version: 0.4.5
 
    	Author: Colin Walker
 
 	*/
-
 
 	function landr_custom_meta() {
 		add_meta_box( 'landr_meta', 'Like and Replies', 'landr_meta_callback', 'post', 'normal', 'high' );
@@ -31,6 +30,7 @@
 	}
 
 
+	add_action( 'save_post', 'save_landr_urls' );
 
 	function save_landr_urls( $post_id ) {
 
@@ -98,7 +98,40 @@
         	wp_update_post( $updated_post );
 		add_action( 'save_post', 'save_landr_urls' );
 
+		return $content;
+
 	}
-	add_action( 'save_post', 'save_landr_urls' );
+
+
+add_filter( 'wp_insert_post_data', 'filter_post_data', '99', 2 );
+
+function filter_post_data( $content, $postarr ) {
+  
+	$post_id = $postarr["ID"];
+	$types = array ( 'Liked', 'Reply' );
+
+	foreach ( $types as $type ) {
+		$meta = get_post_meta( $post_id, $type, true );
+		if ( $meta != '' ) {
+			$meta_url = esc_url( $meta );			
+			$url = wp_remote_get($meta_url);
+      			$str = wp_remote_retrieve_body( $url );
+      			$str = trim(preg_replace('/\s+/', ' ', $str));
+      			preg_match("/\<title\>(.*)\<\/title\>/i",$str,$pagetitle);
+
+			if ( $type == 'Liked' ) {
+				$mentionstr = '<p><em>Liked: <a class="u-like-of" href="' . $meta_url . '">' . $pagetitle[1] . '</a>...</em></p>';
+			} else {
+				$mentionstr = '<p><em>In reply to: <a class="u-in-reply-to" href="' . $meta_url . '">' . $pagetitle[1] . '</a>...</em></p>';
+			}
+
+			$content['post_content'] = $mentionstr . $content['post_content'];
+			$content['post_content_filtered'] = $content['post_content'];
+			delete_post_meta( $post_id, 'Liked', $meta );
+		}
+	}
+
+	return $content;
+}
 
 ?>
